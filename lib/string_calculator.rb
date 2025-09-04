6df2c4aede38246f20beb58d9ce647fcad08b5f0
root <root@HP>
2025-09-04 14:35:58 +0530

    support custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,9 +2,14 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.size == 1
-    nums = numbers.split(/,|\n/).map(&:to_i)
-    negatives = nums.select { |n| n < 0 }
-    raise "negative numbers not allowed #{negatives.join(",")}" if negatives.any?
-    nums.sum
+    if numbers.start_with?("//")
+			delimiter, numbers = numbers[2..].split("\n", 2)
+			nums = numbers.split(delimiter).map(&:to_i)
+		else
+			nums = numbers.split(/,|\n/).map(&:to_i)
+			negatives = nums.select { |n| n < 0 }
+			raise "negative numbers not allowed #{negatives.join(",")}" if negatives.any?
+		end
+		nums.sum
   end
 end
